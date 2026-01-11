f57dada8d0d6be4e34706067980cc19c7e65e4b4
termul488-ai <termul488@gmail.com>
2026-01-11 23:52:58 +0700

    Update siwak.py

--- siwak.py
+++ siwak.py
@@ -3,25 +3,27 @@
 import os
 import sys
 import time
-import fade
 import asyncio
 import aiohttp
+import fade
 
 # Clear command prompt based on the operating system
 if os.name == "nt":  # Windows
     os.system("cls")
 else:  # Unix/Linux/Mac
     os.system("clear")
+
 attemps = 0
-print("""
-\033[33m                                                                               
+logo = """
 ╔══════════════════════════════════════════════════════╗
 ║\033[33m                ~ H U D A I R U L  A L - A Q S H A ~          \033[31m║
 ║\033[32m                    I N T E R N A L  S C R I P T              \033[31m║
 ║\033[96m                           By: Aby'Walidein                   \033[31m║
 ║\033[37m                               ——o0o——                        \033[31m║
 ╚══════════════════════════════════════════════════════╝033[0m
-""")
+"""
+faded_text = fade.fire(logo)
+print(faded_text)
 while attemps < 100:
     username = input("\033[38;5;2mUsername: \033[0m")
     password = input("\033[38;5;2mPassword: \033[0m")
@@ -48,8 +50,8 @@ async def increment_view_count(session):
                 time.sleep(0.01)
                 print("\033[38;5;2mInfo target \033[33m" +str(url)+ " \033[35work\033[0m")          
     except aiohttp.ClientError as e:
-                time.sleep(0.01)
-                print("\033[48;5;1mInfo target \033[0m \033[38;5;3m" +str(url)+ "  \033[38;5;7mMaybe down!\033[0m")
+            time.sleep(0.01)
+            print("\033[48;5;1mInfo target \033[0m \033[38;5;3m" +str(url)+ "  \033[38;5;7mMaybe down!\033[0m")
 
 async def main():
     connector = aiohttp.TCPConnector(limit=None)  # Enable connection pooling
